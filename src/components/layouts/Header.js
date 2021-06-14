31dd61385dda57d3639050992bb762f7f57b1e81
v-nguyen6 <73795871+v-nguyen6@users.noreply.github.com>
2021-06-14 13:56:21 -0700

    react imports set up for header page (hamburger, headroom, custom icons)

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -9,10 +9,13 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 // Custom React Components 
-import Headroom from "react-headroom";
-
-
-
+// - Headroom: Hide/Show header while scrolling 
+import Headroom from "react-headroom"; 
+// - Hamburger Menu (controls the creation and animation of hamburger menu)
+import { Twirl as Hamburger } from "hamburger-react";
+// - Custom React Icons (Custom Icon Library)
+import { FaTwitter, FaGithub, FaLinkedIn } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 //--------------------------------------------------------------
 const Header = () => {
